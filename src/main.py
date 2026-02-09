c0810efe872e47ee1b9b8c4314dcbbff3c1d53fd
Ali Abbas <120602595+AliAbbasGabol@users.noreply.github.com>
2026-02-09 22:01:50 +0500

    updating few new stuff

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
-from fastapi import FastAPI, Query, HTTPException, status, Path
+from fastapi import FastAPI, Query, HTTPException, status, Path, Body, Header, Response
 from enum import Enum
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl, EmailStr
+from fastapi.responses import JSONResponse, RedirectResponse
 from typing import Annotated
 app = FastAPI()
 
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
 
 class product(BaseModel):
+    model_config = {"extra": "forbid"}
     name: str
-    price: int = Field(ge=1, le=999999)
+    price: int = Field(ge=1, le=999999,examples= ["price must be under 100,000"])
     category: str
-    creator: str | None = None
+    creator: str | None = Field(default=None, examples=["your name"])
+    image: list[Image] | None = None
+   
 
 class category_select(str, Enum):
     food = "alexer"
@@ -39,12 +47,68 @@ def namer(category: category_select):
     
 
 @app.post("/product")
-def add_product(item: product):
+def add_product(item: product,user_agent: Annotated[str | None, Header()] = None):
     items = item.model_dump()
     items.update({"price": 133})
     return items
 
 @app.put("/producs/{id}" )
-def update_product(id: Annotated[int, Path(title="The ID of the item to get")], price: Annotated[int, Query(ge=1, le=999999)], phone: Annotated[int, Query(ge=100000000000, le=9999999999999)], email: Annotated[str, Query(pattern= "^[a-zA-z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$")]):
+def update_product(id: Annotated[int, Path(title="The ID of the item to get")], price: Annotated[int, Query(ge=1, le=999999)], phone: Annotated[int, Query(ge=100000000000, le=9999999999999)], email: Annotated[str, Query(pattern= "^[a-zA-z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$")], item: Annotated[
+        product,
+        Body(
+           openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        ),
+    ],):
     return price, id
      
+
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user: UserIn) -> any:
+    return user
+
+
+app.get("/portal")
+async def get_portal(teleport: bool = False) -> Response:
+    if teleport:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return JSONResponse(content={"message": "Here's your interdimensional portal."})
